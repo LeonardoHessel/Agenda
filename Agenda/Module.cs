5b2019dd96a40db07e247c7772c660a7ff89978d
Leonardo Hessel <52008953+LeonardoHessel@users.noreply.github.com>
2020-10-10 00:18:31 -0300

    Clases e DER - Home

---
+++ Agenda/Module.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class Module:Connection
+    {
+        public long ID;
+        public string Name;
+        public bool Restrict;
+
+        public static Module GetModule(long module_id)
+        {
+            Module module = new Module();
+            string sql = "SELECT * FROM `module` WHERE `id` = @id";
+            module.TextCommand(sql);
+            module.AddParameter("id", module_id);
+
+            if (module.ExecuteQuery())
+            {
+                return module.TableToList(Connection.SelectedTable)[0];
+            }
+            return null;
+        }
+
+        public static List<Module> GetModules()
+        {
+            Module module = new Module();
+            string sql = "SELECT * FROM `module`";
+            module.TextCommand(sql);
+
+            if (module.ExecuteQuery())
+            {
+                return module.TableToList(Connection.SelectedTable);
+            }
+            return null;
+        }
+        
+        public bool Save()
+        {
+            string sql = "UPDATE `module` SET `name` = @name, `restrict` = @restrict WHERE `id` = @id";
+        }
+
+        private List<Module> TableToList(DataTable table)
+        {
+            if (table != null)
+            {
+                List<Module> modules = new List<Module>();
+                modules = (from DataRow row in table.AsEnumerable()
+                           select new Module()
+                           {
+                               ID = Convert.ToInt64(row["id"]),
+                               Name = row["name"].ToString(),
+                               Restrict = Convert.ToBoolean(row["restrict"]),
+                           }).ToList();
+                return modules;
+            }
+            return null;
+        }
+    }
+}
